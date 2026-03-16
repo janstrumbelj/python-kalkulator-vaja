8fb0d393f0143343688e8e4f02247538f6acfb70
Jan Štrumbelj <jan.strumbelj@gmail.com>
2026-03-16 19:23:56 +0100

    Izboljšan izpis rezultatov z enotami

--- main.py
+++ main.py
@@ -1,51 +1,49 @@
 def sestej(a, b):
-    return a + b
+    return f"\nVsota izračuna števil {a} + {b} = {a + b}"
 
 def odstej(a, b):
-    return a - b
+    return f"\nRazlika izračuna števil {a} - {b} = {a - b}"
 
 def pomnozi(a,b):
-    return a * b
+    return f"\nProdukt izračuna števil {a} * {b} = {a * b}"
 
 def deli(a,b):
     try:
         a / b
     except ZeroDivisionError:
-        return "Deljivec ne sme biti števila 0"    
+        return "\nDrugo število ne sme biti 0"    
     else:
-        return a / b
-
-
+        return f"\nKoličnik izračuna števil {a} : {b} = {a / b}"
 
 while True:
 
-    print("Pozdrav! Mini kalkulator")
+    print("\nPozdrav! Mini kalkulator")
     print("1 = seštevanje")
     print("2 = odštevanje")
     print("3 = množenje")
     print("4 = deljenje")
     print("0 = izhod")
 
-    izbira = input("Kaj želiš narediti? (1-4): ")
+    izbira = input("Kaj želiš narediti? (0-4): ")
 
     if izbira == "1":
         x = float(input("Prvo število: "))
         y = float(input("Drugo število: "))
-        print(f"Rezultat: {sestej(x, y)}")
+        print(sestej(x, y))
     elif izbira == "2":
         x = float(input("Prvo število: "))
         y = float(input("Drugo število: "))
-        print(f"Rezultat: {odstej(x, y)}")
+        print(odstej(x, y))
     elif izbira == "3":
         x = float(input("Prvo število: "))
         y = float(input("Drugo število: "))
-        print(f"Rezultat: {pomnozi(x, y)}")
+        print(pomnozi(x, y))
     elif izbira == "4":
         x = float(input("Prvo število: "))
         y = float(input("Drugo število: "))
-        print(f"Rezultat: {deli(x, y)}")
+        print(deli(x, y))
     elif izbira == "0":
-        print("Zapustili ste meni.")
+        print("\nUspešno ste zapustili meni.\n")
         break
     else:
-        print("Neveljavna izbira!")
+        print("\nNeveljavna izbira!")
